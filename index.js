6f788a0b953f7d465775fb29848b36fff28f7cda
Gregory Hoyl Jr <ghjunior@gmail.com>
2011-11-06 21:14:07 +1300

    expose everyauth via exports

--- index.js
+++ index.js
@@ -81,4 +81,7 @@ exports = module.exports = function plugin (schema, opts) {
   // Adds dynamic helpers such as loggedIn,
   // accessible from the views
   exports.helpExpress = everyauth.helpExpress.bind(everyauth);
+
+	// Expose everyauth
+	exports.everyauth = everyauth;
 };
